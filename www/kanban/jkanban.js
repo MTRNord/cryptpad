27cdafe1b6a8f5ce43ed64571a63ee63765e213d
MTRNord <mtrnord1@gmail.com>
2018-07-31 19:09:21 +0200

    Minimize gutter

--- www/kanban/jkanban.js
+++ www/kanban/jkanban.js
@@ -48,8 +48,8 @@
                 this.buttonContent = '+';
                 defaults = {
                     element: '',
-                    gutter: '15px',
-                    widthBoard: '250px',
+                    gutter: '5px',
+                    widthBoard: '300px',
                     responsive: '700',
                     colors: ["yellow", "green", "blue", "red", "orange"],
                     responsivePercentage: false,
