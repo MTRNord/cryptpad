e5d7401ad5358e19b1064a77fa5cb6f101846096
MTRNord <mtrnord1@gmail.com>
2018-07-31 19:48:27 +0200

    Debug

--- www/kanban/inner.js
+++ www/kanban/inner.js
@@ -119,6 +119,7 @@ define([
                 }
             },
             click: function (el) {
+                console.log("click!")
                 if (framework.isReadOnly() || framework.isLocked()) { return; }
                 if (kanban.inEditMode) {
                     $(el).focus();
